a85c9e9c44a5baad588714828c617681f06b5103
Christian Lewe <ch_lewe@agdsn.me>
2023-11-09 17:38:56 +0100

    Nix: Fix nixpkgs to 23.05

--- shell.nix
+++ shell.nix
@@ -1,4 +1,7 @@
-{ pkgs ? import <nixpkgs> {}
+{ pkgs ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/4ecab3273592f27479a583fb6d975d4aba3486fe.tar.gz";
+    sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
+  }) {}
 }:
 let
   python3 = pkgs.python3.withPackages (p: with p; [
